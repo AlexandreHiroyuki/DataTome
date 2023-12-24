e7506e316bdb2250016e57408ef772563ce9ab13
AlexandreHiroyuki <alex.hiroyuki@outlook.com>
2023-12-24 02:44:46 -0300

    feat(DataTomeAnalysis): :sparkles: add analysis class
    #4

--- src/DataTomeAnalysis.h
+++ src/DataTomeAnalysis.h
@@ -9,6 +9,42 @@
 
 #include <DataTomeMvAvg.h>
 
+#include <cmath>
 #include <cstdlib>
+
+template <typename TypeOfArray, typename TypeOfSum = TypeOfArray>
+class DataTomeAnalysis : public DataTomeMvAvg<TypeOfArray, TypeOfSum> {
+ private:
+ public:
+  DataTomeAnalysis(size_t size) : DataTomeMvAvg<TypeOfArray, TypeOfSum>(size) {}
+
+  TypeOfArray var() {
+    TypeOfArray average = this->get();
+
+    TypeOfArray var = 0;
+    for (size_t i = 0; i < this->point_count(); i++) {
+      var += ((*this)[i] - average) * ((*this)[i] - average);
+    }
+
+    return var;
+  }
+
+  TypeOfArray std() { return sqrt(var() / this->size()); }
+
+  TypeOfArray partial_var(size_t partial_id) {
+    TypeOfArray average = this->partial_get(partial_id);
+
+    TypeOfArray var = 0;
+    for (size_t i = 0; i < this->partial_size(partial_id); i++) {
+      var += ((*this)[i] - average) * ((*this)[i] - average);
+    }
+
+    return var;
+  }
+
+  TypeOfArray partial_std(size_t partial_id) {
+    return sqrt(partial_var(partial_id) / this->partial_size(partial_id));
+  }
+};
 
 #endif  // DATA_TOME_ANALYSIS_H
