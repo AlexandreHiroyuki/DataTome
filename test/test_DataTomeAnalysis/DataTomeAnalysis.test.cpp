c98ed9d2178f392a7efc47017eb75de80c6c2785
Alexandre Hiroyuki <alex.hiroyuki@outlook.com>
2024-04-09 08:44:14 -0300

    style(DataTomeAnalysis): :art: rename analysis instances to be more meaningful on tests

--- test/test_DataTomeAnalysis/DataTomeAnalysis.test.cpp
+++ test/test_DataTomeAnalysis/DataTomeAnalysis.test.cpp
@@ -10,42 +10,42 @@ long int data[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
 long int mode_data[10] = {1, 1, 2, 3, 3, 3, 3, 4, 4, 5};
 
 void test_minAndMax(void) {
-  DataTomeAnalysis<int, long int> TestMV(10);
+  DataTomeAnalysis<int, long int> TestAnalysis(10);
 
   for (size_t i = 0; i < 10; i++) {
-    TestMV.push(data[i]);
+    TestAnalysis.push(data[i]);
   }
 
-  TEST_ASSERT_EQUAL(data[0], TestMV.min());
-  TEST_ASSERT_EQUAL(data[9], TestMV.max());
+  TEST_ASSERT_EQUAL(data[0], TestAnalysis.min());
+  TEST_ASSERT_EQUAL(data[9], TestAnalysis.max());
 }
 
 void test_median(void) {
-  DataTomeAnalysis<int, long int> TestMV(10);
+  DataTomeAnalysis<int, long int> TestAnalysis(10);
 
   for (size_t i = 0; i < 10; i++) {
-    TestMV.push(data[i]);
+    TestAnalysis.push(data[i]);
   }
 
-  TEST_ASSERT_EQUAL((data[4] + data[5]) / 2, TestMV.median());
+  TEST_ASSERT_EQUAL((data[4] + data[5]) / 2, TestAnalysis.median());
 }
 
 void test_lowestMode(void) {
-  DataTomeAnalysis<int, long int> TestMV(10);
+  DataTomeAnalysis<int, long int> TestAnalysis(10);
 
   for (size_t i = 0; i < 10; i++) {
-    TestMV.push(mode_data[i]);
+    TestAnalysis.push(mode_data[i]);
   }
 
-  TEST_ASSERT_EQUAL(3, TestMV.lowest_mode());
+  TEST_ASSERT_EQUAL(3, TestAnalysis.lowest_mode());
 }
 
 void test_getVarianceAndStandardDeviation(void) {
-  DataTomeAnalysis<int, long int> TestMV(10);
+  DataTomeAnalysis<int, long int> TestAnalysis(10);
   size_t data_count = 5;
 
   for (size_t i = 0; i < data_count; i++) {
-    TestMV.push(data[i]);
+    TestAnalysis.push(data[i]);
   }
 
   long int mock_sum = 0;
@@ -54,70 +54,70 @@ void test_getVarianceAndStandardDeviation(void) {
   }
 
   long int mock_average = mock_sum / data_count;
-  TEST_ASSERT_EQUAL(mock_average, TestMV.get());
+  TEST_ASSERT_EQUAL(mock_average, TestAnalysis.get());
 
   long int mock_var = 0;
   for (size_t i = 0; i < data_count; i++) {
     mock_var += (data[i] - mock_average) * (data[i] - mock_average);
   }
 
-  TEST_ASSERT_EQUAL(mock_var, TestMV.var());
+  TEST_ASSERT_EQUAL(mock_var, TestAnalysis.var());
 
-  TEST_ASSERT_EQUAL(sqrt(mock_var / data_count), TestMV.std());
+  TEST_ASSERT_EQUAL(sqrt(mock_var / data_count), TestAnalysis.std());
 }
 
 void test_partialMinAndMax(void) {
-  DataTomeAnalysis<int, long int> TestMV(10);
+  DataTomeAnalysis<int, long int> TestAnalysis(10);
   size_t partial_size = 3;
   size_t data_count = 5;
 
-  size_t partial_id = TestMV.partial_create(partial_size);
+  size_t partial_id = TestAnalysis.partial_create(partial_size);
 
   for (size_t i = 0; i < data_count; i++) {
-    TestMV.push(data[i]);
+    TestAnalysis.push(data[i]);
   }
 
-  TEST_ASSERT_EQUAL(data[2], TestMV.partial_min(partial_id));
-  TEST_ASSERT_EQUAL(data[4], TestMV.partial_max(partial_id));
+  TEST_ASSERT_EQUAL(data[2], TestAnalysis.partial_min(partial_id));
+  TEST_ASSERT_EQUAL(data[4], TestAnalysis.partial_max(partial_id));
 }
 
 void test_partialMedian(void) {
-  DataTomeAnalysis<int, long int> TestMV(10);
+  DataTomeAnalysis<int, long int> TestAnalysis(10);
   size_t partial_size = 3;
   size_t data_count = 5;
 
-  size_t partial_id = TestMV.partial_create(partial_size);
+  size_t partial_id = TestAnalysis.partial_create(partial_size);
 
   for (size_t i = 0; i < data_count; i++) {
-    TestMV.push(data[i]);
+    TestAnalysis.push(data[i]);
   }
 
-  TEST_ASSERT_EQUAL(data[3], TestMV.partial_median(partial_id));
+  TEST_ASSERT_EQUAL(data[3], TestAnalysis.partial_median(partial_id));
 }
 
 void test_partialLowestMode(void) {
-  DataTomeAnalysis<int, long int> TestMV(10);
+  DataTomeAnalysis<int, long int> TestAnalysis(10);
   size_t partial_size = 5;
   size_t data_count = 5;
 
-  size_t partial_id = TestMV.partial_create(partial_size);
+  size_t partial_id = TestAnalysis.partial_create(partial_size);
 
   for (size_t i = 0; i < data_count; i++) {
-    TestMV.push(mode_data[i]);
+    TestAnalysis.push(mode_data[i]);
   }
 
-  TEST_ASSERT_EQUAL(1, TestMV.partial_lowest_mode(partial_id));
+  TEST_ASSERT_EQUAL(1, TestAnalysis.partial_lowest_mode(partial_id));
 }
 
 void test_partialVarianceAndStandardDeviation(void) {
-  DataTomeAnalysis<int, long int> TestMV(10);
+  DataTomeAnalysis<int, long int> TestAnalysis(10);
   size_t partial_size = 3;
   size_t data_count = 5;
 
-  size_t partial_id = TestMV.partial_create(partial_size);
+  size_t partial_id = TestAnalysis.partial_create(partial_size);
 
   for (size_t i = 0; i < data_count; i++) {
-    TestMV.push(data[i]);
+    TestAnalysis.push(data[i]);
   }
 
   long int mock_sum = 0;
@@ -126,17 +126,17 @@ void test_partialVarianceAndStandardDeviation(void) {
   }
 
   long int mock_average = mock_sum / partial_size;
-  TEST_ASSERT_EQUAL(mock_average, TestMV.partial_get(partial_id));
+  TEST_ASSERT_EQUAL(mock_average, TestAnalysis.partial_get(partial_id));
 
   long int mock_var = 0;
   for (size_t i = data_count - 1; i > data_count - partial_size - 1; i--) {
     mock_var += (data[i] - mock_average) * (data[i] - mock_average);
   }
 
-  TEST_ASSERT_EQUAL(mock_var, TestMV.partial_var(partial_id));
+  TEST_ASSERT_EQUAL(mock_var, TestAnalysis.partial_var(partial_id));
 
   TEST_ASSERT_EQUAL(sqrt(mock_var / partial_size),
-                    TestMV.partial_std(partial_id));
+                    TestAnalysis.partial_std(partial_id));
 }
 
 void process() {
